c9b2af90d363b8f93076d739db95478c39b4351c
Marcelo Xavier <67488874+devmxs-code@users.noreply.github.com>
2025-06-23 14:26:07 -0300

    Add dark mode configuration to Tailwind CSS setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Habilita o modo escuro baseado em classe
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
